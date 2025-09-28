19674db9b5efaf5c2764cb069a094bfb3d678b6a
BrittanyBrown14 <kyana.brownstar@hotmail.co.uk>
2025-09-28 18:14:05 +0100

    GET THE TITLES

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
 
+    const books = array.map(isBook);
+    console.log(books)
+    return books
 };
+
+function isBook(array2)
+{    
+    // console.log(array2.title)
+    return array2.title;
+
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+// SOLVED
