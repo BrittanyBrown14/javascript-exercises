91cea9adf7d95cb451f509af6e793dfe8b65e762
BrittanyBrown14 <kyana.brownstar@hotmail.co.uk>
2025-09-28 17:22:59 +0100

    Palindromes solvved

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+let newText = text.toLowerCase().replace(/\s/g, "").replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 
+    let rev = newText.split("").reverse().join("");
+console.log(newText);
+console.log(rev)
+
+    if (rev == newText) {
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// SOLVED
