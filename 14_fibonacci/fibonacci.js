3384c34b7d84c99bcb72658acc692f16c2c80267
BrittanyBrown14 <kyana.brownstar@hotmail.co.uk>
2025-09-28 18:02:10 +0100

    FIBONACCI

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+let fibArray = [0, 1];
+if (number < 0) {
+    return "OOPS"
+}
+else
+{
+  for (let index = 2; index < 30; index++) {
+    const element = fibArray[index - 1] + fibArray[index - 2];
+
+    fibArray.push(element);
+    }
+    return fibArray[number];  
+}
 
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+//SOLVED
